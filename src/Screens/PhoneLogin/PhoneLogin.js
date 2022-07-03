588fce2fee0a4eb0f1e8e8e01ac785a95717179a
Mehdihussainmalla <mehdihussainmalla@gmail.com>
2022-07-03 17:23:24 +0530

    sdsd

--- src/Screens/PhoneLogin/PhoneLogin.js
+++ src/Screens/PhoneLogin/PhoneLogin.js
@@ -1,58 +1,39 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import WrapperContainer from '../../Components/WrapperContainer'
 import Button from '../../Components/Button'
-import fontFamily from '../../styles/fontFamily'
 import TextInputComp from '../../Components/TextInputs'
-import { moderateScale, textScale } from '../../styles/responsiveSize'
-import colors from '../../styles/colors'
 import imagePath from '../../navigation/imagePath'
+import { styles } from './style'
 
 const PhoneLogin = () => {
     return (
         <WrapperContainer>
-            <View style={{ marginTop: 30 }}>
-                <View style={{ paddingVertical: 40 }}>
-                   <Image source={imagePath.bgc}/>
-                </View>
+            <View style={styles.bgcimg}>
+                <Image source={imagePath.bgc} />
             </View>
-            <View style={{ marginVertical: moderateScale(10) }}>
-                <Text style={{
-                    fontSize: textScale(25),
-                    paddingLeft: moderateScale(12),
-                    padding: moderateScale(8),
-                    color: colors.main_Color,
-                    fontFamily: fontFamily.BarlowBold
-                }}>Login</Text>
+            <View style={styles.loginheading}>
+                <Text style={styles.logintxt}>Login</Text>
             </View>
-            <View style={{ flexDirection: "row", borderWidth: 0.9 }}>
-                <View style={{ flex: 0.5, paddingVertical: 10 }}>
-                    <Text style={{ alignSelf: "center", fontFamily: fontFamily.BarlowBold }}>Email</Text>
+            <View style={styles.emailphone}>
+                <View style={styles.emailview}>
+                    <Text style={styles.emailtxt}>Email</Text>
                 </View>
-                <View style={{ flex: 0.5, paddingVertical: 10, backgroundColor: colors.main_Color }}>
-                    <Text style={{ alignSelf: "center", fontFamily: fontFamily.BarlowBold, color: colors.white }}>Phone</Text>
+                <View style={styles.phoneview}>
+                    <Text style={styles.phonetxt}>Phone</Text>
                 </View>
             </View>
             <TextInputComp
-                inputview={{ marginVertical: moderateScale(30) }}
+                inputview={styles.inputstyle}
                 placeHolder={"Enter Phobe Number"} />
-            <View style={{ marginVertical: moderateScale(10) }}>
-                <Text style={{
-                    fontSize: textScale(12),
-                    fontFamily: fontFamily.BarlowSemiBold
-                }}>An OTP will be send to entered mobile number</Text>
+            <View style={styles.descstyle}>
+                <Text style={styles.desctxt}>An OTP will be send to entered mobile number</Text>
             </View>
             <Button
                 ButtonTxt={"Proceed"} />
 
-            <View style={{
-                justifyContent: "center",
-                alignItems: "center",
-                marginVertical: moderateScale(10),
-
-
-            }}>
-                <Text style={{ fontFamily: fontFamily.BarlowBold, }}>Login With Social</Text>
+            <View style={styles.socialstyle}>
+                <Text style={styles.socialtxt}>Login With Social</Text>
             </View>
             <Button
                 source={imagePath.facebook_icon}
@@ -63,16 +44,17 @@ const PhoneLogin = () => {
                 source={imagePath.google_icon}
                 ButtonTxt={"Sign in with Google"} />
 
-            <View style={{ flexDirection: "row", justifyContent: "center" }}>
-                <Text style={{ fontFamily: fontFamily.BarlowBold }}>Don't have an account?</Text>
-                <Text style={{ fontSize: textScale(14), color: colors.red, fontFamily: fontFamily.BarlowMedium }}>Sign up</Text>
+            <View style={styles.accountstyle}>
+                <Text style={styles.accountxt}>Don't have an account?</Text>
+                
+                <TouchableOpacity activeOpacity={0.5}>
+                    <Text style={styles.signuptxt}>Sign up</Text>
+                </TouchableOpacity>
             </View>
         </WrapperContainer>
 
     )
 }
-const styles = StyleSheet.create({
 
-})
 
 export default PhoneLogin
---
+++ src/Screens/PhoneLogin/style.js
@@ -0,0 +1,99 @@
+import { StyleSheet } from "react-native";
+import colors from "../../styles/colors";
+import fontFamily from "../../styles/fontFamily";
+import { moderateScale, textScale } from "../../styles/responsiveSize";
+
+export const styles = StyleSheet.create({
+
+    bgcimg:
+    {
+        paddingVertical: 40
+    },
+    loginheading:
+    {
+        marginVertical: moderateScale(10)
+    },
+    logintxt:
+    {
+        fontSize: textScale(25),
+        paddingLeft: moderateScale(12),
+        padding: moderateScale(8),
+        color: colors.main_Color,
+        fontFamily: fontFamily.BarlowBold
+    },
+    emailphone:
+    {
+        flexDirection: "row",
+        borderWidth: 0.9
+    },
+    emailview:
+    {
+        flex: 0.5,
+        paddingVertical: 10
+    },
+    emailtxt:
+    {
+        alignSelf: "center",
+        fontFamily: fontFamily.BarlowBold
+    },
+    phoneview:
+    {
+        flex: 0.5,
+        paddingVertical: moderateScale(10),
+        backgroundColor: colors.main_Color
+    },
+    phonetxt:
+    {
+        alignSelf: "center",
+        fontFamily: fontFamily.BarlowBold,
+        color: colors.white
+    },
+    inputstyle:
+    {
+        marginVertical: moderateScale(30)
+    },
+    descstyle:
+    {
+        marginVertical: moderateScale(10)
+    },
+    desctxt:
+    {
+        fontSize: textScale(12),
+        fontFamily: fontFamily.BarlowSemiBold
+    },
+    socialstyle:
+    {
+        justifyContent: "center",
+        alignItems: "center",
+        marginVertical: moderateScale(10),
+
+
+    },
+    socialtxt:
+    {
+        fontFamily: fontFamily.BarlowBold,
+    },
+    accountstyle:
+    {
+        flexDirection: "row",
+        justifyContent: "center"
+    },
+    accountxt:
+    {
+        fontFamily: fontFamily.BarlowBold
+
+    },
+    signuptxt:
+    {
+        fontSize: textScale(14),
+        color: colors.red,
+        fontFamily: fontFamily.BarlowSemiBold,
+        paddingLeft: moderateScale(5)
+    }
+
+
+
+
+
+
+})
