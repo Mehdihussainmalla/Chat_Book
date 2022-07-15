04ca50a942ea24c1cb0367b66b60d4b4873c812d
CBL-Mac-037 <cbl-mac-037@CBL-Mac-037s-iMac-Pro.local>
2022-07-15 18:54:13 +0530

    ddsd

--- src/Screens/HomeScreen/HomeScreen.js
+++ src/Screens/HomeScreen/HomeScreen.js
@@ -13,15 +13,15 @@ const homeData = [{
   key: 1, profileName: "Mehdi",
   description: "malla", time: "10sec ago",
   postHeader: "post header",
-  postDescription: "postDescription",
-  image: imagePath.bgc
+  postDescription: `To simply describe a beautiful natural scenery you can use adjective words like Pristine, Stunning, Exquisite, Indescribable etc.`,
+  image: imagePath.post_image
 },
 {
-  key: 1, profileName: "hussain",
+  key: 2, profileName: "hussain",
   description: "@hussain", time: "10sec ago",
   postHeader: "post header",
-  postDescription: "postDescription",
-  image: imagePath.bgc
+  postDescription: `To simply describe a beautiful natural scenery you can use adjective words like Pristine, Stunning, Exquisite, Indescribable etc.`,
+  image: imagePath.post_image2
 }]
 
 
@@ -44,10 +44,16 @@ const HomeScreen = ({ navigation }) => {
       <>
         <TouchableOpacity style={{
           marginRight: 15, backgroundColor: "red",
-          paddingHorizontal: 14, marginTop: 10, borderRadius: 0.9,
+          paddingHorizontal: 14,
+          marginTop: 10,
+          borderRadius: 0.9,
           borderRadius: 5
         }}>
-          <Text style={{ fontSize: textScale(16), alignSelf: "center", padding: 4 }} >{item.name}</Text>
+          <Text style={{
+            fontSize: textScale(16),
+            alignSelf: "center",
+            padding: 4
+          }} >{item.name}</Text>
         </TouchableOpacity>
 
       </>
@@ -57,52 +63,100 @@ const HomeScreen = ({ navigation }) => {
 
   const homeRender = ({ item }) => {
     // console.log(item, "item>>>>")
-    return(
+    return (
       <View>
-        
+        <Image
+          style={{}}
+          source={item?.image} />
       </View>
+
+      // <View style={{ flexDirection:"column", justifyContent: "space-between", marginTop: 20 }}>
+      //   <View>
+      //     <View style={{ flexDirection: "row" }}>
+      //       <Image style={{ marginTop: 10 }}
+      //         source={imagePath.Radio_icon} />
+      //       <View style={{ flexDirection: "column", marginLeft: 10 }}>
+      //         <Text style={{ marginTop: 20, }}>{item.profileName}<Text>
+      //           {item?.description}</Text></Text>
+      //         <Text style={{}}>{item?.time}</Text>
+      //         <Text style={{ paddingTop: 30, fontWeight: "700" }}>{item?.postHeader}</Text>
+      //         <View style={{ marginHorizontal: 10, flexDirection: "column", backgroundColor: "red" }}>
+      //           <Text style={{ padding: 5, fontWeight: "500" }}>{item?.postDescription}</Text>
+
+      //           <Image
+      //             style={{}}
+      //             source={item?.image} />
+      //         </View>
+      //       </View>
+
+      //     </View>
+      //     <View style={{ marginLeft: 50 }}>
+      //       <View style={{ flexDirection: "row", justifyContent: "space-evenly", marginTop: 10 }}>
+      //         <View style={{ flexDirection: "row" }}>
+      //           <Image source={imagePath.thumb_up} />
+      //           <Text style={{ paddingLeft: 10 }}>count</Text>
+      //         </View>
+      //         <View style={{ flexDirection: "row" }}>
+      //           <Image
+      //             style={{ marginLeft: 10 }}
+      //             source={imagePath.thumb_up} />
+      //           <Text style={{ paddingLeft: 15 }}>count</Text>
+      //         </View>
+      //         <View style={{ flexDirection: "row" }}>
+      //           <Image
+      //             style={{ marginLeft: 10 }}
+      //             source={imagePath.comment} />
+      //           <Text style={{ paddingLeft: 10 }}>count</Text>
+      //         </View>
+      //         <Image
+      //           style={{ marginLeft: 10 }}
+      //           source={imagePath.share_icon} />
+      //       </View>
+      //       </View>
+      //   </View>
+      //   <Image style={{ tintColor: "green", height: "10%" }} source={imagePath.more_vert} />
+      //  </View>
+
     )
-
   }
-
-
   return (
     <WrapperContainer>
-
-      <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+      <View style={{ flex: 1 }}>
         <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-          <TouchableOpacity>
+          <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+            <TouchableOpacity>
+              <Image
+                style={{ marginTop: 5 }}
+                source={imagePath.HamBurger} />
+            </TouchableOpacity>
+            <Text style={{ fontSize: 16, paddingLeft: 100, marginTop: 8, fontFamily: fontFamily.BarlowSemiBold }}>Home</Text>
+          </View>
+          <TouchableOpacity style={{ marginTop: 10 }} >
             <Image source={imagePath.Radio_icon} />
           </TouchableOpacity>
-          <Text style={{ fontSize: 16, paddingLeft: 100, marginTop: 8, fontFamily: fontFamily.BarlowSemiBold }}>Home</Text>
         </View>
-        <TouchableOpacity style={{ marginTop: 10 }} >
-          <Image source={imagePath.Radio_icon} />
+        <View>
+          <FlatList
+            data={data}
+            horizontal={true}
+            renderItem={renderItem}
+          />
+        </View>
+        <View>
+          <FlatList
+            data={homeData}
+            renderItem={homeRender}
+          />
+        </View>
+
+        <TouchableOpacity
+
+          onPress={signOut}
+          style={{ marginTop: 100 }}
+        >
+          <Text>HomeScreen</Text>
         </TouchableOpacity>
       </View>
-      <View>
-        <FlatList
-          data={data}
-          horizontal={true}
-          renderItem={renderItem}
-        />
-      </View>
-
-      {/* <FlatList
-        data={homeData}
-        renderItem={homeRender}
-      /> */}
-
-      <View>
-        <Text>fiosdf</Text>
-      </View>
-
-      <TouchableOpacity
-
-        onPress={signOut}
-        style={{ marginTop: 100 }}>
-        <Text>HomeScreen</Text>
-      </TouchableOpacity>
     </WrapperContainer>
   )
 }
--- src/constants/imagePath.js
+++ src/constants/imagePath.js
@@ -4,4 +4,13 @@ export default {
     bgc: require("../assets/images/bgc.png"),
     BACK_ARROW: require('../assets/images/ic_back_arrow.png'),
     Radio_icon: require('../assets/images/radio-icon.png'),
+    HamBurger: require("../assets/images/hameburger.png"),
+    more_vert: require("../assets/images/more-vert.png"),
+    thumb_up: require("../assets/images/thumb-up.png"),
+    thumb_down: require("../assets/images/thumb-down.png"),
+    comment: require("../assets/images/comment.png"),
+    share_icon: require("../assets/images/share-icon.png"),
+    post_image: require("../assets/images/postimage.png"),
+    post_image2: require("../assets/images/post2.png"),
+
 }
